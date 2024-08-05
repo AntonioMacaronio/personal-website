678441b650d212267933bbc8ca8f7e4b79eb803d
AntonioMacaronio <antzhan1234@gmail.com>
2024-08-05 08:28:13 -0700

    typo corrections and README update

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # personal-website
 Code containing my personal website!
+
+To deploy, 2 commands are ran:
+1. `npm run build`
+2. `npm run deploy`
--- personal-website/src/AboutMe.js
+++ personal-website/src/AboutMe.js
@@ -18,13 +18,13 @@ const AboutMe = () => {
         Hey y'all! I'm Anthony Zhang, a Computer Science and Applied Mathematics student at UC Berkeley. My interests include computer graphics, 3D reconstruction, photorealistic rendering, machine learning, and making software fast.
       </p>
       <p>
-        Currently, I'm a contributor and developer for Nerfstudio, an open source project for developing and researching Neural Radiance Fields and Gaussian Splatting. Nerfstudio is maintained by KAIR under Berkeley AI Research (BAIR) and other community contributors!
+        Currently, I'm a contributor and developer for <a href="https://docs.nerf.studio/">Nerfstudio</a>, an open source project for developing and researching Neural Radiance Fields and Gaussian Splatting. <a href="https://docs.nerf.studio/">Nerfstudio</a> is maintained by KAIR under Berkeley AI Research (BAIR) and other community contributors!
       </p>
       <p>
         Previously, I was a software engineer intern at Square where I worked on customer platform infrastructure and internal microservices. 
       </p>
       <p>
-        Before that, I was a software engineer intern at Collective Health developing backend APIs and a research assistant and Shanghai J.T University working on medical imaging.
+        Before that, I was a software engineer intern at Collective Health developing backend APIs and a research assistant at Shanghai Jiao Tong University working on medical imaging.
       </p>
       <Link to="/">Back to Minigame</Link>
     </div>
