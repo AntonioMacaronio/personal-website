0e14f6e466d4ff5840991592228e104a33aeaabd
AntonioMacaronio <antzhan1234@gmail.com>
2025-10-01 00:11:43 -0700

    updating about me

--- personal-website/src/AboutMe.js
+++ personal-website/src/AboutMe.js
@@ -17,10 +17,10 @@ const AboutMe = () => {
         <div className="about-me-text">
           <h1>Anthony Zhang</h1>
           <p>
-            I'm an EECS Master's student at UC Berkeley advised by  <a href="https://people.eecs.berkeley.edu/~kanazawa/">Angjoo Kanazawa</a>. My interests include computer graphics, 3D computer vision, scene reconstruction, human motion/humanoid robotics, and machine learning.
+            I'm an EECS Master's student at UC Berkeley advised by <a href="https://people.eecs.berkeley.edu/~kanazawa/">Angjoo Kanazawa</a>. My interests include computer graphics, 3D computer vision, scene reconstruction, human motion/humanoid robotics, and machine learning.
           </p>
           <p>
-            Currently, I'm a contributor and developer for <a href="https://docs.nerf.studio/">Nerfstudio</a>, an open source project in the Berkeley AI Research (BAIR) lab for developing and  3D reconstruction methods such as Neural Radiance Fields and Gaussian Splatting. 
+            Currently, I'm an Applied Scientist intern at Amazon's Frontier Intelligence and Robotics Lab working on humanoid robotics.  I'm also a contributor and developer for <a href="https://docs.nerf.studio/">Nerfstudio</a>, an open source project in the Berkeley AI Research (BAIR) lab for developing and  3D reconstruction methods such as Neural Radiance Fields and Gaussian Splatting. 
           </p>
           <p>
             Previously, I was a software engineer intern at Square where I worked on customer platform infrastructure and internal microservices. 
